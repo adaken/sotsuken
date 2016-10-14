c68151f8bd9b1831cd45975f8588f493147f50d5
kt <kt@kt_vaio>
2016-10-14 12:55:56 +0900

    FFT処理を関数化した FFTグラフのY軸を10に固定

--- svm/accelerationfft.py
+++ svm/accelerationfft.py
@@ -25,44 +25,28 @@ def apply_blackmanwin(a, fs):
     blackman_window = np.blackman(fs)
     return a * blackman_window
 
-if __name__ == '__main__':
+def fft(sheet, col_letter, begin_row, end_row, fft_points):
 
     # 加速度のリストをxlsxから読み込む
-    from util.excelwrapper import ExcelWrapper
-    ws = ExcelWrapper(filename=r"E:\work\bicycle_acc_hirano.xlsx",
-                      sheetname='Sheet4')
-    acc = ws.select_column('F', begin_row=20200, end_row=20455)
+    acc = ws.select_column(col_letter=col_letter, begin_row=begin_row,
+                           end_row=end_row)
 
     # リストを配列に変換
     acc = np.array(acc)
-    print type(acc)
 
-    """
-    # xの値を生成
-    x = np.arange(-3.14, 3.14, 0.25/40.764)
-    y = np.arange(-3.14, 3.14, 0.25/40.764)
-
-    # 高さを計算
-    sin1 = np.sin(3*x)
-    sin2 = np.sin(7*y)
-    print sin1
-    print sin2
-
-    acc = sin1 + sin2
-    """
     # DC成分を取り除く
     acc = acc - np.mean(acc)
 
     # サンプリング周波数(FFTのポイント数)
-    fs = 256
+    fs = fft_points
 
     # サンプリング間隔(サンプリング周波数の逆数)
     ps = 1/float(fs)
 
     # fsとの差を0で埋める
     #acc = np.hstack((acc, np.zeros(fs - acc.size)))
-    print "acc:", acc
-    print "size:", acc.size
+    #print "acc:", acc
+    print "acc_size:", acc.size
 
     # 波形を描画（窓関数なし）
     plt.subplot(221)  # 2行2列のグラフの1番目の位置にプロット
@@ -92,28 +76,48 @@ if __name__ == '__main__':
 
     # X軸 - 周波数
     fftfreq = np.fft.fftfreq(n, ps)
-    print "X軸:", fftfreq
+    #print "X軸:", fftfreq
 
     # FFT
     fftdata = np.fft.fft(acc)[0:n/2]
 
     # Y軸 - スペクトルの絶対値
     fftmag = np.abs(fftdata)
-    print "Y軸:", fftmag
+    #print "Y軸:", fftmag
 
     # スペクトルを描画
     plt.subplot(223)  # 2行2列のグラフの3番目の位置にプロット
     plt.plot(xrange(0, fftmag.size), fftmag)
-    plt.axis([0, fftmag.size, 0, np.max(fftmag)])
+    #plt.axis([0, fftmag.size, 0, np.max(fftmag)])
+    plt.axis([0, fftmag.size, 0, 10])
     plt.xlabel("freq")
     plt.ylabel("spectrum")
 
-
     # 図をグラフとして保存
-    #fig.savefig(r"E:\work\fft_graph.png", dpi=200)
+    #plt.savefig(r"E:\work\fig.png", dpi=200)
 
     # 図を表示
-    plt.show()
+    #plt.show()
+
+    return fftmag
+
+if __name__ == '__main__':
+    from util.excelwrapper import ExcelWrapper
+    ws = ExcelWrapper(filename=r"E:\work\bicycle_acc_hirano.xlsx",
+                      sheetname='Sheet4')
+
+    col_letter = 'F'
+    fft_points = 256
+    begin_row = 20200
+    end_row = begin_row + fft_points - 1
+
+    for i in xrange(5):
+        print "read %s%d:%s%d" % (col_letter, begin_row, col_letter, end_row)
+        fft(sheet=ws, col_letter=col_letter, begin_row=begin_row,
+             end_row=end_row, fft_points=fft_points)
+        begin_row += fft_points
+        end_row = begin_row + fft_points - 1
+        plt.savefig(r"E:\work\fig\fig%03d.png" % i, dpi=200)
 
     """
     ここからSOM処理
