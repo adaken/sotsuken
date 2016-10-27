18f138092f373ec9f773c2c7f967c2be056f38bd
kt <kt@kt_vaio>
2016-10-27 22:11:08 +0900

    マップに文字列をプロットできるようにした

--- svm/accelerationfft.py
+++ svm/accelerationfft.py
@@ -22,37 +22,6 @@ def normalize_scale(arr):
     """
     return (arr - np.min(arr)) / (np.max(arr) - np.min(arr))
 
-def make_grayscaled_map(som_map):
-    if isinstance(som_map, list):
-        som_map = np.array(som_map)
-    assert isinstance(som_map, np.ndarray), "list or ndarray only"
-
-    # すべてのノードに対して近傍ノードとの平均ユークリッド距離を計算
-
-    map_x, map_y, z = som_map.shape
-    # 全ノードの位置
-    x, y = np.meshgrid(np.arange(map_x), np.arange(map_y))
-    nodes_idx = np.c_[x.ravel(), y.ravel()]
-    # ノード[x,y]の近傍ノードの位置
-    n_node_idx = lambda x, y : [[x-1, y+1], [x, y+1], [x+1, y+1], [x+1, y],
-                                [x+1, y-1], [x, y-1], [x-1, y-1], [x-1, y]]
-    # ユークリッド距離
-    #eu_dist = lambda vec_p, vec_q : np.sqrt(np.sum((vec_p - vec_q) ** 2))
-    # 近傍ノードとのユークリッド距離の平均値
-    eu_dis_mean = lambda n_nodes, node : np.mean(np.linalg.norm(n_nodes - node))
-    # 各ノードに対して計算
-    max_shape = np.max(som_map.shape[0])
-    gray_scaled_map = np.zeros(map_x * map_y * 4).reshape(map_x, map_y, 4)
-    for i, j in nodes_idx:
-        n_nodes = []
-        for n_i, n_j in n_node_idx(i, j):
-            if 0 <= n_i < max_shape and 0 <= n_j < max_shape:
-                n_nodes.append(som_map[n_i, n_j])
-        dis_mean = eu_dis_mean(n_nodes, som_map[i, j])
-        gray_scaled_map[i, j, 3] = dis_mean
-    normalized = normalize_scale((normalize_standard(gray_scaled_map)))
-    return normalized
-
 def main():
 
     # xlsxの辞書
@@ -60,22 +29,22 @@ def main():
     xls = {
         'run':(r'E:\work\data\run.xlsx', sheet_name),
         'walk':(r'E:\work\data\walk.xlsx', sheet_name),
-        #'skip':(r'E:\work\data\skip.xlsx', sheet_name)
+        'skip':(r'E:\work\data\skip.xlsx', sheet_name)
     }
     colors = {
-        'run':[255, 0, 0],
-        'skip':[0, 255, 0],
-        'walk':[0, 0, 255]
+        'run':'red',
+        'walk':'blue',
+        'skip':'green'
     }
     fft_points = 256
     column_letter = 'F'
     begin_row = 2
     end_row = lambda begin : begin + fft_points - 1
-    read_count = 100    # xlsx1つを読み込む回数
-    sample_count = 5    # xlsx1つのサンプリング回数
+    read_count = 1      # xlsx1つを読み込む回数
+    sample_count = 10   # xlsx1つのサンプリング回数
     overlap = 0         # 重複サンプリングを許容する行数
-    map_size = (40, 40) # 表示するマップの大きさ
-    train_itr = 2       # 学習ループの回数
+    map_size = (50, 50) # 表示するマップの大きさ
+    train_itr = 1000     # 学習ループの回数
     input_vector = []   # 入力ベクトル
 
     for act, v in xls.items():
@@ -86,32 +55,55 @@ def main():
             end = end_row(begin) # 終了位置
             for j in xrange(sample_count):
                 rows = ws.select_column(column_letter, begin, end, log=True)
-                fftdata = normalize_scale(fft(rows, fft_points))
-                input_vector.append([colors[act], fftdata])
+                fftdata = fft(rows, fft_points, out_fig=False) # FFT
+                fftdata = normalize_scale(fftdata) # 0～1に正規化
+                input_vector.append((act, fftdata))
                 begin += fft_points - overlap # 読み込む範囲を更新
                 end = end_row(begin)
 
-    som = modsom.SOM(map_size, input_vector)
-    output_map = som.train(train_itr)
-    plt.imshow(output_map)
+    som = modsom.SOM(map_size, input_vector, display='gray_scale')
+    som.set_parameter(neighbor=0.25, learning_rate=0.3)
+    map_, label_coord = som.train(train_itr)
+    plt.imshow(map_, interpolation='nearest')
+    for label, coord in label_coord:
+        x, y = coord
+        plt.text(x, y, label, color=colors[label])
     plt.show()
 
-def som_gray_test():
-    vec_size = 1000
+def som_gray_without_label():
+    vec_size = 500
     vec_dim = 128
-    data_type_count = 5
+    data_type_count = 4
     map_size = (40, 40)
-    train_itr = 2
-    patterns =  [np.random.randint(0, 2, vec_dim) for i in xrange(data_type_count)]
+    train_itr = 30
+    patterns = [np.random.random(vec_dim) for i in xrange(data_type_count)]
+    input_vec = [patterns[np.random.randint(data_type_count)] for i in xrange(vec_size)]
+    som = modsom.SOM(map_size, input_vec, display='gray_scale')
+    map_ = som.train(train_itr)
+    print "map_shape", map_.shape
+    plt.imshow(map_, interpolation='nearest')
+    plt.show()
+
+def som_gray_with_label():
+    vec_size = 500
+    vec_dim = 128
+    data_type_count = 8
+    map_size = (40, 40)
+    train_itr = 100
+    # ラベル付き特徴ベクトルのリスト
+    patterns =  [("pattern%d" % (i+1), np.random.randint(0, 2, vec_dim))
+                 for i in xrange(data_type_count)]
     for i, v in enumerate(patterns): print "pattern:%d\n" % (i+1), v
     input_vec = [patterns[np.random.randint(data_type_count)] for i in xrange(vec_size)]
-    som = modsom.SOM(map_size, input_vec)
-    som.set_parameter(neighbor=0.26, learning_rate=0.22)
-    output_map = som.train(train_itr)
-    output_map = make_grayscaled_map(output_map)
-    print "output_map_shape", output_map.shape
-    print "output_map:", output_map
-    plt.imshow(output_map)
+    som = modsom.SOM(map_size, input_vec, display='gray_scale')
+    map_, label_coord = som.train(train_itr)
+    print "map_shape:", map_.shape
+    print "map:\n", map_
+    print "label_coord:\n", label_coord
+    for label, coord in label_coord:
+        x, y = coord
+        plt.text(x, y, label, ha='center', va='center', color='red')
+    plt.imshow(map_, interpolation='nearest')
     plt.show()
 
 def som_color_test():
@@ -127,7 +119,6 @@ def som_color_test():
         print "label:", v[0]
     input_vec = [vec_patterns[np.random.randint(data_type_count)] for i in xrange(vec_size)]
     som = modsom.SOM(map_size, input_vec)
-    som.set_parameter(neighbor=0.26, learning_rate=0.22)
     output_map = som.train(train_itr)
     print "output_map_shape", output_map.shape
     print "output_map:", output_map
@@ -157,14 +148,14 @@ def hirakawa_test():
         print "add to input_vector"
         print "input_vector_size", len(in_vector)
     print "learning map"
-    som_map = modsom.SOM()
-    gray_map = make_grayscaled_map(som_map)
-    plt.imshow(gray_map)
+    map_ = modsom.SOM((40, 40), in_vector, display='gray_scale')
+    plt.imshow(map_, interpolation='nearest')
     plt.show()
 
 if __name__ == '__main__':
 
     main()
-    #som_gray_test()
+    #som_gray_with_label()
+    #som_gray_without_label()
     #som_color_test()
     #hirakawa_test()
--- util/modsom.py
+++ util/modsom.py
@@ -28,23 +28,29 @@ import numpy as np
 from numpy import random as rand
 
 class SOM:
-    def __init__(self, shape, input_data):
+    def __init__(self, shape, input_data, display=None):
         assert isinstance(shape, (int, list, tuple))
         assert isinstance(input_data, (list, np.ndarray))
-        if len(input_data[0]) == 2: # ラベルあり
+        if self._has_label(input_data): # ラベルあり
             print "ラベルあり"
             self.with_label = True
             l, v = self._split_input(input_data)
-            self.input_labels = np.array(l) if isinstance(l, list) else l
+            l_type = self._check_label_type(l)
+            if l_type is 'str':
+                self.input_label_type = l_type
+                self.input_labels = l
+            elif l_type is 'rgb':
+                self.input_label_type = l_type
+                self.input_labels = np.array(l) if isinstance(l, list) else l
             self.input_layer = np.array(v, np.float32) if isinstance(v, list) else v
-        else:                       # ラベルなし
+        else:                           # ラベルなし
             print "ラベルなし"
             self.with_label = False
             self.input_layer = np.array(input_data, dtype=np.float32) \
             if isinstance(input_data, list) else input_data
         input_shape = tuple(self.input_layer.shape)
         assert len(input_shape) == 2
-        self.shape = tuple(shape) # 入力層のサイズ(X x Y)
+        self.shape = tuple(shape)       # 入力層のサイズ(X x Y)
         self.input_num = input_shape[0] # 入力ベクトルの総数
         self.input_dim = input_shape[1] # 入力ベクトルの次元
         # 出力層、平均0分散1のランダム値で初期化
@@ -57,11 +63,7 @@ class SOM:
         self._life = self.input_num * self._param_input_length_ratio
         self._param_neighbor = 0.25
         self._param_learning_rate = 0.1
-
-    def _split_input(self, input_data):
-        labels = [i[0] for i in input_data]
-        vectors = [i[1] for i in input_data]
-        return labels, vectors
+        self.display = display
 
     def set_parameter(self, neighbor=None, learning_rate=None, input_length_ratio=None):
         if neighbor:
@@ -85,14 +87,14 @@ class SOM:
         """勝者ノードを決定"""
         sub = self.output_layer - data
         dis = np.linalg.norm(sub, axis=1) # ユークリッド距離を計算
-        bmu = np.argmin(dis) # 最も距離が近いノードのインデックス
+        bmu = np.argmin(dis)              # 最も距離が近いノードのインデックス
         return np.unravel_index(bmu, self.shape)
 
     def _update(self, bmu, data, i):
         """ノードを更新"""
         dis = np.linalg.norm(self.index_map - bmu, axis=1) # 勝者ノードとの距離
-        L = self._learning_rate(i) # 学習率係数
-        S = self._learning_radius(i, dis) # 学習半径
+        L = self._learning_rate(i)                         # 学習率係数
+        S = self._learning_radius(i, dis)                  # 学習半径
         self.output_layer += L * S[:, np.newaxis] * (data - self.output_layer)
 
     def _learning_rate(self, t):
@@ -107,6 +109,51 @@ class SOM:
         initial = max(self.shape) * self._param_neighbor
         return initial * np.exp(-t/self._life)
 
+    def train(self, n):
+        print "合計ループ回数:", n * self.input_num
+        print "学習状況: 0%",
+        for i in xrange(n):
+            if (i+1)/float(n)*100%10==0: print "%d%%"%((i+1)/float(n)*100),
+            for j in self._random_idx_gen(self.input_num):
+                data = self.input_layer[j] # 入力ベクトル
+                win_idx = self._get_winner_node(data)
+                self._update(win_idx, data, i)
+        print ""
+        return self._return_map()
+
+    def _return_map(self):
+        map_ = self.output_layer.reshape((self.shape[1], self.shape[0], self.input_dim))
+        if self.with_label:
+            if self.display is None:
+                return map_, self._get_str_label()
+            elif self.display is 'gray_scale':
+                return self._make_gray_scale_map(map_), self._get_str_label()
+        else:
+            if self.display is None:
+                return map_
+            elif self.display is 'gray_scale':
+                return self._make_gray_scale_map(map_)
+
+    def _has_label(self, input_data):
+        if len(input_data[0]) == 2 \
+        and isinstance(input_data[0][0], (str, list, np.ndarray)):
+            return True
+        return False
+
+    def _split_input(self, input_data):
+        labels = [i[0] for i in input_data]
+        vectors = [i[1] for i in input_data]
+        return labels, vectors
+
+    def _check_label_type(self, labels):
+        label = labels[0]
+        assert isinstance(label, (list, np.ndarray, str))
+        if isinstance(label, str):
+            return 'str'
+        elif isinstance(label, (list, np.ndarray)):
+            assert len(label) == 3
+            return 'rgb'
+
     def _random_idx_gen(self, n):
         """要素が0からnまでの重複のないランダム値を返すジェネレータ"""
         vacant_idx = range(n)
@@ -115,42 +162,74 @@ class SOM:
             yield vacant_idx[r]
             del vacant_idx[r]
 
-    def _label(self):
-        """出力層のノードをラベリング"""
-        x, y = self.shape
-        rabeled_map = np.ones(x * y * 3).reshape(x, y, 3)
-        for i in xrange(self.input_num):
+    def _get_str_label(self):
+        """
+        出力層のノードとラベルを関連付ける
+
+        Return
+        ------
+        label_cood : tuple(str, ndarray)
+            input vector's label with the coord
+        """
+        label_coord = []
+        for i, data in enumerate(self.input_layer):
             label = self.input_labels[i]
-            data = self.input_layer[i]
             win_idx = self._get_winner_node(data)
-            rabeled_map[win_idx] = label
-        return rabeled_map
+            label_coord.append((label, win_idx))
+        return label_coord
 
-        """
+    def _get_rgb_label(self):
+        """出力層のノードをRGBでラベリング"""
+        # 未完成です
         x, y = self.shape
-        labeled_map = [[[0, 0, 0] for i in xrange(y)] for j in xrange(x)]
-        for i in xrange(self.input_num):
-            label = self.input_labels[i]
-            data = self.input_layer[i]
-            win_i, win_j = self._get_winner_node(data)
-            labeled_map[win_i][win_j] = label
-            self.output_layer[win_idx] = label
-        return labeled_map
-        """
+        rgb_map = np.ones(x * y * 3).reshape(x, y, 3)
+        for rgb, data in zip(self.input_labels, self.input_layer):
+            win_idx = self._get_winner_node(data)
+            rgb_map[win_idx] = rgb
+        return rgb_map
 
-    def train(self, n):
-        print "ループ回数:", self.input_num * n
-        for i in range(n):
-            print "学習%d回目" % (i + 1)
-            for j in self._random_idx_gen(self.input_num):
-                data = self.input_layer[j] # 入力ベクトル
-                win_idx = self._get_winner_node(data)
-                self._update(win_idx, data, i)
-        #return self.output_layer.reshape(self.shape + (self.input_dim,))
-        if self.with_label:
-            return self._label()
-        else:
-            return self.output_layer.reshape((self.shape[1], self.shape[0], self.input_dim))
+    def _normalize_standard(self, arr):
+        """
+        正規化方法1
+        (Xi - "Xの平均") / "Xの標準偏差" で平均0分散1にする
+        """
+        return (arr - np.mean(arr)) / np.std(arr)
+
+    def _normalize_scale(self, arr):
+        """
+        正規化方法2
+        (Xi - Xmin) / (Xmax - Xmin) で0<Xi<1にする
+        """
+        return (arr - np.min(arr)) / (np.max(arr) - np.min(arr))
+
+    def _make_gray_scale_map(self, map_):
+
+        # すべてのノードに対して近傍ノードとの平均ユークリッド距離を計算
+
+        map_x, map_y, z = map_.shape
+        x, y = np.meshgrid(np.arange(map_x), np.arange(map_y)) # 全ノードのインデックス
+        nodes_idx = np.c_[x.ravel(), y.ravel()]
+        # ノード[x,y]の近傍ノードの位置
+        n_node_idx = lambda x, y : [[x-1, y+1], [x, y+1], [x+1, y+1], [x+1, y],
+                                    [x+1, y-1], [x, y-1], [x-1, y-1], [x-1, y]]
+        # ユークリッド距離
+        #eu_dist = lambda vec_p, vec_q : np.sqrt(np.sum((vec_p - vec_q) ** 2))
+        # 近傍ノードとのユークリッド距離の平均値
+        eu_dis_mean = lambda n_nodes, node : np.mean(np.linalg.norm(n_nodes - node))
+        # 各ノードに対して計算
+        gray_map = np.zeros(map_x * map_y * 4).reshape(map_x, map_y, 4)
+        for i, j in nodes_idx:
+            n_nodes = []
+            for n_i, n_j in n_node_idx(i, j):
+                if 0 <= n_i < map_x and 0 <= n_j < map_y:
+                    n_nodes.append(map_[n_i, n_j])
+            dis_mean = eu_dis_mean(n_nodes, map_[i, j])
+            gray_map[i, j, 3] = dis_mean
+        #normalized = normalize_scale(normalize_standard(gray_scaled_map))
+        #normalized = normalize_standard(normalize_scale(gray_scaled_map))
+        #normalized = normalize_standard(gray_scaled_map)
+        normalized = self._normalize_scale(gray_map)
+        return normalized
 
 if __name__ == '__main__':
     pass
