393fe4800b7bb3307abbd7111e14c50971c319da
locked <locked@papaya>
2016-11-10 12:31:46 +0900

    test

--- svm/svm.py
+++ svm/svm.py
@@ -8,15 +8,15 @@ def svm_test1():
     param = svm_parameter('-s 0 -t 2')
     m = svm_train(prob, param)
     test_label, test_data = [1, 1, -1, -1], [[]]
-    
+
 def main():
     # データを読み込む
     def read_data():
         pass
     # スケーリング
-        
-    # 
-    
+
+    #a
+
 if __name__ == '__main__':
     prob = svm_problem([-1,1,1,-1], [[0,0], [0,1],[1,0],[1,1]])    # 教師データ (XOR)
     param = svm_parameter('-s 0 -t 2 -c 1')    # パラメータ (C-SVC, RBF カーネル, C=1)
