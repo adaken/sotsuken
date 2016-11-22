d05201415010fc20ffcb592901d2d73d634f70de
kt <kt@kt_vaio>
2016-11-22 18:41:59 +0900

    commit

---
+++ default/pilttest.py
@@ -0,0 +1,18 @@
+'''
+Created on 2016/11/22
+
+@author: kt
+'''
+
+if __name__ == '__main__':
+    
+    def drow_circle(rgb, size, savepath):
+        assert isinstance(rgb, tuple)
+        assert isinstance(size, tuple)
+        from PIL import Image
+        from PIL import ImageDraw
+        im= Image.new('RGBA', size, (0, 0, 0, 0))
+        draw = ImageDraw.Draw(im)
+        draw.ellipse(((1, 1), size, outline=None, fill=rgb)
+        del draw
+        im.save(savepath)
