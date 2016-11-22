bd933a7e4d165e862ff266b5598a846a817344a5
kt <kt@kt_vaio>
2016-11-22 18:45:56 +0900

    bugfix

--- util/util.py
+++ util/util.py
@@ -147,7 +147,7 @@ def drow_circle(rgb, size, savepath):
     from PIL import ImageDraw
     im= Image.new('RGBA', size, (0, 0, 0, 0))
     draw = ImageDraw.Draw(im)
-    draw.ellipse(((1, 1), size, outline=None, fill=rgb)
+    draw.ellipse(((1, 1), size), outline=None, fill=rgb)
     del draw
     im.save(savepath)
 
