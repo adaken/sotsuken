23d0ca99186d757ca147a28bce4350e4b2168dd7
locked <locked@banana>
2016-11-24 19:48:38 +0900

    アクションとそれに対応するアイコンを指定できるようにした

--- kml/kmlwrapper.py
+++ kml/kmlwrapper.py
@@ -10,29 +10,74 @@ class KmlWrapper:
     def createAnimeKml(self,
                        save_path,
                        times,
-                       longitudes,
-                       latitudes,
-                       format_time = True,
-                       sampling_interval = 0, # 0 to no interval
-                       icon_res = 'http://maps.google.com/mapfiles/kml/shapes/man.png',
-                       icon_scale = 1):
+                       lons,
+                       lats,
+                       acts=None,
+                       act_icons=None,
+                       icon_res='http://maps.google.com/mapfiles/kml/shapes/man.png',
+                       icon_scale=1,
+                       format_time=True,
+                       sampling_step=1):
         """
-        時間、経度、緯度の配列からアニメーション可能なkmlを生成します
+        時間、経度、緯度、アクションのリストからアニメーション可能なkmlを生成します
+
+        Parameters
+        ----------
+        save_path : str
+            kmlを保存するパス
+
+        times, lons, lats, acts : list or tuple
+            これらはすべて同じサイズのリストでなければならない。
+            time : strs
+                時間のリスト
+            lons : floats
+                経度のリスト
+            lats : floats
+                緯度のリスト
+            acts : strs, default: None
+                その時間におけるアクション名のリスト
+
+        act_icons : dic, default:None
+            アクション名を指定した際に使われる、アクション名をkeyとするアイコンリソースの辞書
+            {str(アクション名) : str(アクション名に対応するアイコンリソース)}
+
+        icon_res : str, default:'http://maps.google.com/mapfiles/kml/shapes/man.png':
+            アクションを指定しない場合のアイコンリソース
+
+        icon_scale : int, default: 1
+            アイコンの大きさ
+
+        format_time : bool, default: True
+            時間のフォーマットを変更するかどうか
+
+        sampling_step : int, default: 1
+            step > 0
+            読み込みペース
+            指定した分だけ次の読み込み行番号を増やす
         """
 
-        # リストのサイズが同じかチェック
-        if not len(times) == len(longitudes) == len(latitudes):
-            print "Error: times, longitude and latitudes must be same size ndarray"
-            return
+        # アクションの指定、リストのサイズが同じかをチェック
+        if not (acts is None and act_icons is None):
+            print "アクション指定あり"
+            if acts is None:
+                raise AssertionError("act_iconsを指定した場合、actsも指定する必要があります。")
+            elif act_icons is None:
+                raise AssertionError("actsを指定した場合、act_iconsも指定する必要があります。")
+            assert len(times) == len(lons) == len(lats) == len(acts), "引数times, lons, lats, actsのサイズが違います。これらのサイズをそろえる必要があります。"
+        else:
+            print "アクション指定なし"
+            assert len(times) == len(lons) == len(lats), "引数times, lons, latsのサイズが違います。これらのサイズをそろえる必要があります。"
 
         # リソースが利用可能かチェック
-        if not self.__available_resorce(icon_res): return
+        if not self._is_resorce_available(icon_res):
+            raise AssertionError("リソースが存在しません: {}".format(icon_res))
 
         # タイムスタンプの形式を変更
-        print "changing timestamp format..."
         if format_time:
-            times = self.__format_time(times)
+            print "タイムスタンプの形式を変更中です..."
+            times = self._format_times(times)
 
+        """
         # <IconStyle>
         icon = simk.Icon(href=icon_res)
         hotspot = simk.HotSpot(x=0, xunits=simk.Units.fraction,
@@ -43,22 +88,69 @@ class KmlWrapper:
         sharedstyle = simk.Style(iconstyle=iconstyle)
 
         # kml生成
-        print "generating kml..."
+        print "kmlを作成中です..."
         kml = simk.Kml()
-        for i in xrange(0, len(times), sampling_interval + 1):
+        for i in xrange(0, len(times), sampling_step):
 
             # <Placemark> <Point> <coordinates>
-            pnt = kml.newpoint(coords=[(longitudes[i], latitudes[i])])
+            pnt = kml.newpoint(coords=[(lons[i], lats[i])])
             pnt.style = sharedstyle
 
             # <TimeStamp> <when>
             pnt.timestamp.when = times[i]
 
         # ファイルに出力
-        print "saving kml..."
+        print "kmlを保存中です..."
         kml.save(save_path, True)
+        """
 
-    def __available_resorce(self, res):
+        print "kmlを作成中です..."
+        kml = simk.Kml()
+        hotspot = simk.HotSpot(x=0, xunits=simk.Units.fraction,
+                               y=0, yunits=simk.Units.fraction)
+
+        # アクションを指定しなかった場合
+        if acts is None:
+
+            # <IconStyle>
+            icon = simk.Icon(href=icon_res)
+            iconstyle = simk.IconStyle(icon=icon, scale=icon_scale, hotspot=hotspot)
+
+            # 共通の<Style>
+            sharedstyle = simk.Style(iconstyle=iconstyle)
+
+            for i in xrange(0, len(times), sampling_step):
+
+                # <Placemark> <Point> <coordinates>
+                pnt = kml.newpoint(coords=[(lons[i], lats[i])])
+                pnt.style = sharedstyle
+
+                # <TimeStamp> <when>
+                pnt.timestamp.when = times[i]
+
+        # アクションを指定した場合
+        else:
+
+            # <IconStyle>
+            sharedstyles = {act:simk.Style(simk.IconStyle(icon=simk.Icon(href=icon),
+                                                          scale=icon_scale, hotspot=hotspot))
+                            for act, icon in act_icons.items()}
+
+            for i in xrange(0, len(times), sampling_step):
+
+                # <Placemark> <Point> <coordinates>
+                pnt = kml.newpoint(coords=[(lons[i], lats[i])])
+                pnt.style = sharedstyles[acts[i]]
+
+                # <TimeStamp> <when>
+                pnt.timestamp.when = times[i]
+
+        # ファイルに出力
+        print "kmlを保存中です..."
+        kml.save(save_path, True)
+        print "保存が完了しました"
+
+    def _is_resorce_available(self, res):
         """
         ファイルの存在とかをチェック
         """
@@ -73,20 +165,33 @@ class KmlWrapper:
         if os.path.exists(res):
             return True
         else:
-            print "Error: input resorce is not exist: '%s'" % res
             return False
 
-    def __format_time(self, times):
+    def _format_times(self, times):
         """
         タイムスタンプの形式を整える
         yyyy/m/d hh:mm:ss.mmm -> yyyy-mm-dd"T"hh:mm:ss.mmm"Z"
         "%Y-%m-%d %H:%M:%S.%f" # 元の形式
         "%Y-%m-%d %H:%M:%S" # microsecondが0のときの形式
         """
-
         format_new = "%Y-%m-%dT%H:%M:%S.%fZ" # 新しい形式
-
         return [t.strftime(format_new) for t in times]
 
 if __name__ == "__main__":
-    pass
+    from util.excelwrapper import ExcelWrapper
+    from util.util import drow_circle
+    import random
+    icon_size = (16, 16)
+    act_res = {'run' :drow_circle(rgb=(255, 0, 0), size=icon_size, savepath=r'E:\tmp\run.png'),
+               'jump':drow_circle(rgb=(0, 255, 0), size=icon_size, savepath=r'E:\tmp\jump.png'),
+               'walk':drow_circle(rgb=(0, 0, 255), size=icon_size, savepath=r'E:\tmp\walk.png')}
+    act_res_keys = act_res.keys()
+    br = 9
+    ws = ExcelWrapper(filename=r"E:\work\bicycle_gps_hirano.xlsx", sheetname='Sheet1')
+    times = ws.select_column(column_letter='A', begin_row=br, log=True)
+    lats  = ws.select_column(column_letter='J', begin_row=br, log=True)
+    lons  = ws.select_column(column_letter='K', begin_row=br, log=True)
+    acts = [act_res_keys[random.randint(0, 2)] for i in xrange(len(times))]
+    kml = KmlWrapper()
+    kml.createAnimeKml(save_path=r'E:\test.kml', times=times, lons=lons, lats=lats, acts=acts,
+                       act_icons=act_res, icon_scale=0.3, format_time=True, sampling_step=10)
