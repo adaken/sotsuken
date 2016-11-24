d556899cc6782467b9922a2700e474f21da90bbc
locked <locked@banana>
2016-11-24 19:49:11 +0900

    bugfix

--- util/util.py
+++ util/util.py
@@ -132,34 +132,39 @@ def make_input_from_xlsx(filename,
 def drow_circle(rgb, size, savepath):
     """
     円の画像(png)を作成
-    
+
     Parameters
     ----------
-    rgb : tuple
+    rgb : tuple or list
         円の色
-    size : tuple
+    size : tuple or list
         円のサイズ
     savepath : str
         画像を保存するパス
     """
-    assert isinstance(rgb, tuple)
-    assert isinstance(size, tuple)
-    assert isinstance(savepath, str)
     from PIL import Image
     from PIL import ImageDraw
+    import os
+    assert isinstance(rgb, (tuple, list))
+    assert isinstance(size, (tuple, list))
+    assert isinstance(savepath, str)
+    if os.path.exists(savepath): print "すでにファイルが存在するため上書きします: {}".format(savepath)
+    if isinstance(rgb, list): rgb = tuple(rgb)
+    if isinstance(size, list): size = tuple(size)
     im= Image.new('RGBA', size, (0, 0, 0, 0))
     draw = ImageDraw.Draw(im)
     draw.ellipse(((1, 1), size), outline=None, fill=rgb)
     del draw
     im.save(savepath)
-    
+    return savepath
+
 def drow_random_color_circle(size, savepath):
     """
     ランダムな色の円(png)を作成
-    
+
     Parameters
     ----------
-    size : tuple
+    size : tuple or list
         円のサイズ
     savepath : str
         画像を保存するパス
