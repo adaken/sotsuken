bb8583469ba3516289af159ce5db10a3f8ae6985
locked <locked@phoenix>
2016-12-20 18:04:19 +0900

    joblib_fix

--- app/scikit/test_joblib.py
+++ app/scikit/test_joblib.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from util.util import make_input_from_xlsx
+from app.util.inputmaker import make_input
 import random
 from collections import namedtuple
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report
 from sklearn.externals import joblib
+from app import R, T, L
+
 
 if __name__ == '__main__':
     """
@@ -14,36 +16,38 @@ if __name__ == '__main__':
     """
     Xl = namedtuple('Xl', 'filename, sheet, letter, label, sampling, overlap')
     xls =  (
-         Xl(r'E:\work\data\run_1122_data.xlsx', 'Sheet1', 'F', 'run', 'std', 0),
-         Xl(r'E:\work\data\walk_1122_data.xlsx', 'Sheet1', 'F', 'walk', 'std', 0),
-         Xl(r'E:\work\data\jump_128p_174data_fixed.xlsx', 'Sheet', 'A', 'jump', 'std', 0),
-        #Xl(r'E:\work\data\skip.xlsx', 'Sheet4', 'F', 4, 'rand', 0)
+         Xl(R(r'data\raw\run_1122_data.xlsx'), ['Sheet1'], 'F', 'run', 'std', 0),
+         Xl(R(r'data\raw\walk_1122_data.xlsx'), ['Sheet1'], 'F', 'walk', 'std', 0),
+         Xl(R(r'data\raw\jump_128p_174data_fixed.xlsx'), ['Sheet'], 'A', 'jump', 'std', 0),
+        #Xl(R(r'data\skip.xlsx'), 'Sheet4', 'F', 4, 'rand', 0)
         )
-    input_data = []
-    test_data = []
+    test_vecs = []
+    test_labels = []
     for xl in xls:
-        test_vec = make_input_from_xlsx(filename=xl.filename, sheetname=xl.sheet,
-                                               col=xl.letter, read_range=(12802, None), overlap=xl.overlap,
-                                               sampling=xl.sampling, sample_cnt=21, fft_N=128,
-                                               normalizing='01', label=xl.label, log=False)
-        test_data += test_vec
+        test_vec, test_label = make_input(xlsx=xl.filename, sheetnames=xl.sheet,col=xl.letter,
+                                                min_row=12802,fft_N=128, sample_cnt=21,
+                                                label=xl.label,sampling=xl.sampling,
+                                                overlap=xl.overlap,normalizing='01', log=False)
+        map(test_vecs.append, test_vec)
+        test_labels += test_label
 
-    random.shuffle(test_data)
+    from app.util.inputmaker import random_input_iter
+    test_vecs1, test_labels1 = [], []
+    for i, j in random_input_iter(test_vecs, test_labels):
+        test_vecs1.append(i)
+        test_labels1.append(j)
 
-    test_labels = [vec[0] for vec in test_data]
-    test_vecs = [list(vec[1]) for vec in test_data]
-
-    clf = joblib.load('E:\clf.pkl')
-    test_pred = clf.predict(test_vecs)  #他クラス分類器One-versus-oneによる識別
+    clf = joblib.load(R('misc\model\clf.pkl'))
+    test_pred = clf.predict(test_vecs1)  #他クラス分類器One-versus-oneによる識別
 
     #confusion matrix（ラベルの分類表。分類性能が高いほど対角線に値が集まる）
-    print confusion_matrix(test_labels, test_pred)
+    print confusion_matrix(test_labels1, test_pred)
 
     #分類結果 適合率 再現率 F値の表示
-    print classification_report(test_labels, test_pred)
+    print classification_report(test_labels1, test_pred)
 
     #正答率 分類ラベル/正解ラベル
-    print accuracy_score(test_labels, test_pred)
+    print accuracy_score(test_labels1, test_pred)
 
-    print test_labels       #分類前ラベル
+    print test_labels1       #分類前ラベル
     print list(test_pred)   #One-against-restによる識別ラベル
