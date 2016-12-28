ec8ba4c0fa54b1d2ee5478d7819e7b8ec1c16c2c
kt <kt@kt_vaio>
2016-12-28 21:35:08 +0900

    commit

--- app/util/__init__.py
+++ app/util/__init__.py
@@ -4,5 +4,5 @@ from fft import fft_iter, fftn
 from iconmaker import drow_circle, drow_random_color_circle
 from inputmaker import make_input, random_input_iter
 from normalize import scale_zero_one, standardize
-from timecounter import timecounter
-from jsonio import save_input_to_json, get_input_from_json
+from util import random_idx_gen, timecounter
+from jsonio import save_inputs_as_json, save_features_as_json
