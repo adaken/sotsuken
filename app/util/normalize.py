a5f26669e55a6eaaa50f5993d925537c5f7956ed
kt <kt@kt_vaio>
2016-12-28 21:37:26 +0900

    bugfix

--- app/util/normalize.py
+++ app/util/normalize.py
@@ -2,32 +2,52 @@
 
 import numpy as np
 
-def standardize(arr):
-    """
-    正規化方法1
+def standardize(arr, axis=None):
+    """標準化
+
     (Xi - "Xの平均") / "Xの標準偏差" で平均0分散1にする
+
     """
 
-    assert isinstance(arr,np.ndarray)
-    assert arr.ndim in (1, 2)
-    dim = arr.ndim
-    if dim == 1:
-        return (arr - np.mean(arr)) / np.std(arr)
-    else:
-        return np.array([standardize(a) for a in arr])
+    assert isinstance(arr, np.ndarray)
 
-def scale_zero_one(arr):
-    """
-    正規化方法2
+    mean_ = np.mean(arr, axis)
+    std_ = np.std(arr, axis)
+    if axis is not None: # 次元を揃える
+        s = arr.shape
+        n_shape = s[:axis] + (1,) + s[axis+1:]
+        mean_ = np.reshape(mean_, n_shape)
+        std_ = np.reshape(std_, n_shape)
+    return (arr - mean_) / std_
+
+def scale_zero_one(arr, axis=None):
+    """正規化
+
     (Xi - Xmin) / (Xmax - Xmin) で0<Xi<1にする
+
     """
-    assert isinstance(arr,np.ndarray)
-    assert arr.ndim in (1, 2)
-    dim = arr.ndim
-    if dim == 1:
-        return (arr - np.min(arr)) / (np.max(arr) - np.min(arr))
-    else:
-        return np.array([scale_zero_one(a) for a in arr])
+
+    assert isinstance(arr, np.ndarray)
+
+    max_ = np.max(arr, axis)
+    min_ = np.min(arr, axis)
+    if axis is not None: # 次元を揃える
+        s = arr.shape
+        n_shape = s[:axis] + (1,) + s[axis+1:]
+        max_ = np.reshape(max_, n_shape)
+        min_ = np.reshape(min_, n_shape)
+    return (arr - min_) / (max_ - min_)
 
 if __name__ == '__main__':
-    pass
+    a = np.array([[[1, 2, 3], [4, 5, 0], [5, 4, 6]],
+                  [[4, 8, 6], [6, 5, 9], [1, 3, 2]],
+                  [[9, 5, 2], [6, 6, 2], [9, 8, 9]],
+                  [[5, 6, 4], [3, 4, 1], [1 ,7, 4]]], dtype=np.float64)
+    a = np.random.rand(100, 3, 3)
+    #print a
+    #print "axis0\n{}".format(scale_zero_one(a, axis=0))
+    #print "axis1\n{}".format(scale_zero_one(a, axis=1))
+    print "axis2\n{}".format(scale_zero_one(a, axis=2))
+    #print "axisNone:\n{}".format(scale_zero_one(a, axis=None))
+
+    #print "axis0\n{}".format(standardize(a, axis=0))
